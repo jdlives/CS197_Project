46c7a18712987b3915e6ddd916739c9324ab4802
jdlives <jlaborada.97@gmail.com>
2016-10-04 21:28:21 +0800

    Commit na may gumagana na camera. :) Please run para macheck kung tama.

--- src/rommel/Controller.java
+++ src/rommel/Controller.java
@@ -1,27 +1,54 @@
-//comment niyo changes
+
 package rommel;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
 
 public class Controller {
+	@FXML
+	public Button setAt1;
+	@FXML
+	public Button setAt2;
+	@FXML
+	public void loadSecond(ActionEvent event) throws IOException{
+		Stage stage;
+		Parent root;
+		stage=(Stage) setAt2.getScene().getWindow();
+		root = FXMLLoader.load(getClass().getResource("WebCamPreview.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		stage.setFullScreen(true);
+		
+	}
+	@FXML
 	public void buttonClicked(){
 		System.out.println("Stop it.");
 	}
 	ObservableList<String> officeToVisitList=FXCollections.observableArrayList("ITDC","DCS","OUR");
 	@FXML
 	private ChoiceBox<String> visitBox;
-	private Button XSetAttendance;
 	
-	@FXML
-	private void initialize(){
-		visitBox.setValue("ITDC");
-		visitBox.setItems(officeToVisitList);
-	}
 	public void setClicked(){
 		System.out.print("Hello");
+	}
+	public void initialize() {
+		// TODO Auto-generated method stub
+		visitBox.setValue("ITDC");
+		visitBox.setItems(officeToVisitList);
+		
 	}
 }
